56d06eed9718542bb9ddabe70b0df63b4446e143
Thomas <145207423+trobert-tsp@users.noreply.github.com>
2024-01-15 16:47:20 +0100

    Remove unused crates, fix MacOS extension list

--- src/utility.rs
+++ src/utility.rs
@@ -3,30 +3,22 @@ use ash::extensions::khr::Win32Surface;
 #[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
 use ash::extensions::khr::XlibSurface;
 #[cfg(target_os = "macos")]
-use ash::extensions::mvk::MacOSSurface;
+use ash::extensions::ext::MetalSurface;
+#[cfg(target_os = "macos")]
+use ash::extensions::khr::GetPhysicalDeviceProperties2;
 
 use ash::extensions::ext::DebugUtils;
 use ash::extensions::khr::Surface;
 
 #[cfg(target_os = "macos")]
-use cocoa::appkit::{NSView, NSWindow};
-#[cfg(target_os = "macos")]
-use cocoa::base::id as cocoa_id;
-#[cfg(target_os = "macos")]
-use metal::CAMetalLayer;
-#[cfg(target_os = "macos")]
-use objc::runtime::YES;
-
-// Constants
-const WINDOW_TITLE: &'static str = "Test";
-const WINDOW_WIDTH: u32 = 800;
-const WINDOW_HEIGHT: u32 = 600;
-
-#[cfg(target_os = "macos")]
 pub fn required_extension_names() -> Vec<*const i8> {
+    use ash::vk;
+
     vec![
         Surface::name().as_ptr(),
-        MacOSSurface::name().as_ptr(),
+        MetalSurface::name().as_ptr(),
+        GetPhysicalDeviceProperties2::name().as_ptr(),
+        vk::KhrPortabilityEnumerationFn::name().as_ptr(),
         DebugUtils::name().as_ptr(),
     ]
 }
