eafc944f65c43cb38327eadb8b47ed93cae5103f
kStor2poche <leo.lagarrigue@orange.fr>
2024-03-18 16:10:15 +0100

    Wayland fix

--- src/utility.rs
+++ src/utility.rs
@@ -2,6 +2,7 @@
 use ash::extensions::khr::Win32Surface;
 #[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
 use ash::extensions::khr::XlibSurface;
+use ash::extensions::khr::WaylandSurface;
 #[cfg(target_os = "macos")]
 use ash::extensions::ext::MetalSurface;
 #[cfg(target_os = "macos")]
@@ -37,6 +38,7 @@ pub fn required_extension_names() -> Vec<*const i8> {
     vec![
         Surface::name().as_ptr(),
         XlibSurface::name().as_ptr(),
+        WaylandSurface::name().as_ptr(),
         DebugUtils::name().as_ptr(),
     ]
 }
